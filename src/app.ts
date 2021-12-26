f589ce1911b26afaf1df0a7317df1af9c0c27695
Hariharan <hariharansriram17@gmail.com>
2021-12-26 13:46:05 +0000

    Updated README.md and project

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+import {testController} from "./Controllers/testController.js";
+import express from "express";
+const app = express();
+app.listen(3000, () => console.log("Server started on port 3000"));
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import {testController} from "./Controllers/testController.js";
-console.log('Hello World!');
-testController();
-console.log('Goodbye World!');
